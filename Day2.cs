407753b0633b731a57ce4a3e339903c813e60cd3
Robin Thüs <robinthues@googlemail.com>
2016-10-11 14:07:59 +0200

    solution for day 2
    again part 1 and 2

---
+++ Day2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AdventOfCode2015Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int order = 0, ribbon = 0;
+            int lwNeeds, whNeeds, hlNeeds;
+            int[] sideSizes;
+            string[] inputLines;
+            string path = @"C:\Users\robin\OneDrive\AoC\input2.txt";
+            inputLines = File.ReadAllLines(path);
+            foreach(string line in inputLines)
+            {
+                sideSizes = Array.ConvertAll(line.Split(new char[] {'x'}), int.Parse); // split l w h
+                lwNeeds = 2 * sideSizes[0] * sideSizes[1]; // 2*l*w
+                whNeeds = 2 * sideSizes[1] * sideSizes[2]; // 2*w*h
+                hlNeeds = 2 * sideSizes[2] * sideSizes[0]; // 2*h*l
+                order += lwNeeds+whNeeds+hlNeeds+new int[]{lwNeeds/2, whNeeds/2, hlNeeds/2}.Min(); // extra
+
+                ribbon += sideSizes[0] * sideSizes[1] * sideSizes[2]; // bow
+                IEnumerable<int> orderedSides = sideSizes.OrderBy(num => num); // order sides by size
+                ribbon += 2 * orderedSides.ElementAt(0) + 2 * orderedSides.ElementAt(1); // use the two smallest sides
+            }
+            Console.WriteLine("wrapping paper neede:" + order);
+            Console.WriteLine("ribbon needed:" + ribbon);
+            Console.ReadLine();
+        }
+    }
+}
